914b5fd09def1e5ca2d263c7ed182fa0b55ece51
Gaoyuan (Barry) Wu <62172232+GaoyuanWu@users.noreply.github.com>
2022-10-21 17:09:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JaxSSO",
+    version="0.0.1",
+    author="Gaoyuan Wu",
+    author_email="gaoyuanw@princeton.edu",
+    description="A framework for structural shape optimization based on automatic differentiation (AD) and the adjoint method, enabled by JAX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GaoyuanWu/JaxSSO",
+    packages=setuptools.find_packages(include=['JaxSSO']),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'jax[cpu]'
+    ],
+    extras_require = {},
+    include_package_data = True,
+    python_requires=">=3.7",
+)
